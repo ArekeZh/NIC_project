2980c865ea8d80b6a392a181047a73327ba7ae93
Arlan Zh <150828332+ArekeZh@users.noreply.github.com>
2025-09-19 13:08:29 +0500

    Update views.py

--- views.py
+++ views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
+def main_menu(request):
+    return HttpResponse("Just a main page, enter '/ping' or '/sum?a=1&b=2' to get sm response")
+
 def ping(request):
     return JsonResponse({"message": "pong"})
 
 def sum(request):
-    a = int(request.GET.get('a', 0))
-    b = int(request.GET.get('b', 0))
+    a = int(request.GET.get('a', 2))
+    b = int(request.GET.get('b', 3))
     
+
     return JsonResponse({"result": a + b})
