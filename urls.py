59abb6a9d2fda68d5980188f9025b11f79717ffb
Arlan Zh <150828332+ArekeZh@users.noreply.github.com>
2025-09-19 13:02:39 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main_page.urls')),
+    path('', include('main_page.app_urls')),
     path('admin/', admin.site.urls),
 ]
+
